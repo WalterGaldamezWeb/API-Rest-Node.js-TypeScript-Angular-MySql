9134e1962ec5f933bbedae2de8be8785fe7d1550
WalterGaldamezWeb <walter.galdamez.06@gmail.com>
2020-10-18 11:25:24 -0600

    servidor configurado

--- server/build/index.js
+++ server/build/index.js
@@ -1,2 +1,30 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+class Server {
+    constructor() {
+        //Inicializar express
+        this.app = express_1.default();
+        this.config();
+        this.routes();
+    }
+    //Configurar el puerto del servidor
+    config() {
+        //si existen un puerto en el servidor lo usa sino usara el 3000
+        this.app.set('port', process.env.PORT || 3000);
+    }
+    routes() {
+    }
+    //Inicializar el servidor
+    start() {
+        this.app.listen(this.app.get('port'), () => {
+            console.log("Servidor en el purto", this.app.get('port'));
+        });
+    }
+}
+// Inicializar el Servidor
+const SERVER = new Server();
+SERVER.start();
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,31 @@
-import express from 'express';
+import express, { Application } from 'express';
+
+class Server {
+
+    app : Application;
+
+    constructor () {
+        //Inicializar express
+        this.app = express();
+        this.config();
+        this.routes();
+    }
+    //Configurar el puerto del servidor
+    config () :void {
+        //si existen un puerto en el servidor lo usa sino usara el 3000
+        this.app.set('port', process.env.PORT || 3000);
+    }
+
+    routes () : void {
+
+    }
+    //Inicializar el servidor
+    start () : void {
+        this.app.listen(this.app.get('port'), ()=>{
+            console.log("Servidor en el purto", this.app.get('port'));
+        });
+    }
+}
+// Inicializar el Servidor
+const SERVER = new Server();
+SERVER.start();
