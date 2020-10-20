7c37271b6d038104485c737d2c1956066e060325
WalterGaldamezWeb <walter.galdamez.06@gmail.com>
2020-10-20 10:47:59 -0600

    endpoints modificados consultas SQL a la Base de Datos

--- server/build/controladores/productosControlador.js
+++ server/build/controladores/productosControlador.js
@@ -1,20 +1,56 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const databaseConexion_1 = __importDefault(require("../databaseConexion"));
 class ProductosControlador {
     todosProductos(req, res) {
-        res.json({ text: 'Listado de productos' });
+        return __awaiter(this, void 0, void 0, function* () {
+            const productos = yield databaseConexion_1.default.query('SELECT * FROM productos');
+            res.json(productos);
+        });
     }
     productoId(req, res) {
-        res.json({ text: 'producto ' + req.params.id });
+        return __awaiter(this, void 0, void 0, function* () {
+            const { id } = req.params;
+            const producto = yield databaseConexion_1.default.query('SELECT * FROM productos WHERE id = ?', [id]);
+            //console.log(producto);
+            if (producto.length > 0) {
+                return res.json(producto[0]);
+            }
+            res.status(404).json({ text: 'El producto no existe' });
+            //res.json({text:'producto ' + req.params.id });
+        });
     }
     crearProducto(req, res) {
-        res.json({ text: 'producto creado' });
+        return __awaiter(this, void 0, void 0, function* () {
+            yield databaseConexion_1.default.query('INSERT INTO productos SET ?', [req.body]);
+            res.json({ text: 'producto creado' });
+        });
     }
     actualizarProducto(req, res) {
-        res.json({ text: 'producto actualizado' + req.params.id });
+        return __awaiter(this, void 0, void 0, function* () {
+            const { id } = req.params;
+            yield databaseConexion_1.default.query('UPDATE productos SET ? WHERE id = ?', [req.body, id]);
+            res.json({ text: 'producto actualizado' });
+        });
     }
     eliminarProducto(req, res) {
-        res.json({ text: 'producto eliminado' + req.params.id });
+        return __awaiter(this, void 0, void 0, function* () {
+            const { id } = req.params;
+            yield databaseConexion_1.default.query('DELETE FROM productos WHERE id = ?', [id]);
+            res.json({ text: 'producto eliminado' });
+        });
     }
 }
 const PRODUCTOS_CONTROLADOR = new ProductosControlador();
--- server/src/controladores/productosControlador.ts
+++ server/src/controladores/productosControlador.ts
@@ -3,24 +3,37 @@ import db from '../databaseConexion';
 
 class ProductosControlador {
 
-    public todosProductos (req:Request, res:Response) {
-        res.json({text:'Listado de productos'});
+    public async todosProductos (req:Request, res:Response) {
+        const productos = await db.query('SELECT * FROM productos');
+        res.json(productos);
     }
 
-    public productoId (req:Request, res:Response) {
-        res.json({text:'producto ' + req.params.id });
+    public async productoId (req:Request, res:Response): Promise<any> {
+        const {id} = req.params;
+        const producto = await db.query('SELECT * FROM productos WHERE id = ?', [id]);
+        //console.log(producto);
+        if (producto.length > 0) {
+            return res.json(producto[0]);
+        }
+        res.status(404).json({text:'El producto no existe'});
+        //res.json({text:'producto ' + req.params.id });
     }
 
-    public crearProducto (req:Request, res:Response) {
+    public async crearProducto (req:Request, res:Response): Promise<void> {
+        await db.query('INSERT INTO productos SET ?', [req.body]);
         res.json({text:'producto creado'});
     }
 
-    public actualizarProducto (req:Request, res:Response) {
-        res.json({text:'producto actualizado' + req.params.id});
+    public async actualizarProducto (req:Request, res:Response):Promise<void> {
+        const {id} = req.params;
+        await db.query('UPDATE productos SET ? WHERE id = ?', [req.body , id]);
+        res.json({text:'producto actualizado'});
     }
 
-    public eliminarProducto (req:Request, res:Response) {
-        res.json({text:'producto eliminado' + req.params.id});
+    public async eliminarProducto (req:Request, res:Response): Promise<void> {
+        const {id} = req.params;
+        await db.query('DELETE FROM productos WHERE id = ?', [id]);
+        res.json({text:'producto eliminado'});
     }
 }
 
