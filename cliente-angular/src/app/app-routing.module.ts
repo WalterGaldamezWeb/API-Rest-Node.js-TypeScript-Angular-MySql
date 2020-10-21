2b8bd029c65a424695a09a08829f9ec82e9890b3
WalterGaldamezWeb <walter.galdamez.06@gmail.com>
2020-10-20 18:47:19 -0600

    rutas configuradas en app-routing y navbar

--- cliente-angular/src/app/app-routing.module.ts
+++ cliente-angular/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListaProductosComponent } from './componentes/lista-productos/lista-productos.component';
+import { FormularioProductosComponent } from './componentes/formulario-productos/formulario-productos.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/productos',
+    pathMatch:'full'
+  },
+  {
+    path:'productos',
+    component: ListaProductosComponent
+  },
+  {
+    path: 'agregar-productos',
+    component: FormularioProductosComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
