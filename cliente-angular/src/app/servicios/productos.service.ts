eb7d42a05fae6c584747875772efeb47af90316a
WalterGaldamezWeb <walter.galdamez.06@gmail.com>
2020-10-20 21:17:43 -0600

    metodos del servicio definidos

--- cliente-angular/src/app/servicios/productos.service.ts
+++ cliente-angular/src/app/servicios/productos.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Producto } from '../modelos/producto';
+
 
 
 @Injectable({
@@ -7,6 +9,30 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ProductosService {
 
-  constructor(private productoServicio:ProductosService) { }
+  API_URI = 'http://localhost/api';
+
+  constructor(private productoServicio:ProductosService,
+              private http:HttpClient
+    ) { }
+
+  obtenerTodosProductos () {
+    return this.http.get(`${this.API_URI}/productos`);
+  }
+
+  obtenerProductoId (id:string) {
+    return this.http.get(`${this.API_URI}/productos/${id}`);
+  }
+
+  guardarProducto (producto:Producto) {
+    return this.http.post(`${this.API_URI}/productos`, producto);
+  }
+
+  actualizarProducto (id:string, producto:Producto) {
+    return this.http.put(`${this.API_URI}/productos/${id}`, producto);
+  }
+
+  eliminarProducto (id:string) {
+    return this.http.delete(`${this.API_URI}/productos/${id}`);
+  }
 
 }
